f8a2cc0f0e4695717ae9e7bd36c6f7e6d6a54fc5
varunsrik <92026319+varunsrik@users.noreply.github.com>
2024-08-26 14:26:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -185,7 +185,7 @@ with tab5:
         close_df = result[0]
         volume_df = result[1]
         volume_series = volume_df.iloc[-1]
-        st.write(volume_series)
+        st.write(volume_df)
         today_datetime = pd.Timestamp(dt.datetime.today(),  tz='Asia/Kolkata')
         st.header(f'Live Momentum Screen for {today_datetime.strftime('%H:%M')}, {today_datetime.day_name()}, {str(today_datetime.day)}, {today_datetime.month_name()}, {str(today_datetime.year)}')
         st.subheader('Nifty 500 List')
