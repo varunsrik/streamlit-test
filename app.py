99f2e00285bced852959eb5bcb91f9c3ef2bdbd9
varunsrik <92026319+varunsrik@users.noreply.github.com>
2024-09-04 13:13:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -241,7 +241,7 @@ with tab6:
     st.write(benchmark_dict[benchmark])
     yf_sector_list = [sector_dict[sector] for sector in sectors]
     st.write(yf_sector_list)
-    yf_sector_list = yf_sector_list.append(benchmark_dict[benchmark])
+    yf_sector_list.append(benchmark_dict[benchmark])
     st.write(yf_sector_list)
     prices = yf.download(yf_sector_list, start=start_date, end=end_date)['Adj Close']
   
