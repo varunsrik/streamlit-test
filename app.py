ded07b981d18c0f12914cae1df3a7416813c031e
varunsrik <92026319+varunsrik@users.noreply.github.com>
2024-09-05 13:37:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -293,15 +293,15 @@ with tab6:
                       xref="x", yref="y",
                       fillcolor="lightgreen", opacity=0.3, layer="below", line_width=0)
     
-    fig_rrg.add_shape(type="rect", x0=100, y0=100, x1=1.02*(relative_strength.max().max()), y1=1.02*(momentum.min().min()),
+    fig_rrg.add_shape(type="rect", x0=100, y0=100, x1=1.02*(relative_strength.max().max()), y1=0.98*(momentum.min().min()),
                       xref="x", yref="y",
                       fillcolor="yellow", opacity=0.3, layer="below", line_width=0)
     
-    fig_rrg.add_shape(type="rect", x0=100, y0=100, x1=1.02*(relative_strength.min().min()), y1=1.02*(momentum.min().min()),
+    fig_rrg.add_shape(type="rect", x0=100, y0=100, x1=0.98*(relative_strength.min().min()), y1=0.98*(momentum.min().min()),
                       xref="x", yref="y",
                       fillcolor="lightcoral", opacity=0.3, layer="below", line_width=0)
     
-    fig_rrg.add_shape(type="rect", x0=100, y0=100, x1=1.02*(relative_strength.min().min()), y1=1.02*(momentum.max().max()),
+    fig_rrg.add_shape(type="rect", x0=100, y0=100, x1=0.98*(relative_strength.min().min()), y1=1.02*(momentum.max().max()),
                       xref="x", yref="y",
                       fillcolor="lightblue", opacity=0.3, layer="below", line_width=0)
     
